7da4e1a093129c071aa0d306a75fbd11cdca2bca
appsaprinsky <app1.n.saprinsky@gmail.com>
2022-03-29 05:59:05 +0600

    Part4

--- myApp.js
+++ myApp.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+app.use('/public', express.static(__dirname + "/public"))
+
 app.get('/', function routeHandler(req, res) {
   // res.send('Hello Express');
   res.sendFile(__dirname + '/views/index.html');
