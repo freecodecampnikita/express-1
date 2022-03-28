09bcfc9452ea3f083456cf3e707180ac2af9b865
appsaprinsky <app1.n.saprinsky@gmail.com>
2022-03-29 05:41:50 +0600

    Part2

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function routeHandler(req, res) {
-  res.send('Response String');
+  res.send('Hello Express');
 });
 
 console.log("Hello World");
