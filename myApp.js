e0b254b6d425356a2d68c07d80430c86c9107a35
appsaprinsky <app1.n.saprinsky@gmail.com>
2022-03-29 19:49:30 +0600

    Part7

--- myApp.js
+++ myApp.js
@@ -4,6 +4,16 @@ require('dotenv').config()
 
 app.use('/public', express.static(__dirname + "/public"))
 
+
+app.use( function middleware(req, res, next) {
+  // Do something
+  // Call the next function in line:
+  var string = req.method + " " + req.path + " - " + req.ip;
+  console.log(string);
+  next();
+});
+
+
 app.get('/', function routeHandler(req, res) {
   // res.send('Hello Express');
   res.sendFile(__dirname + '/views/index.html');
@@ -22,12 +32,11 @@ app.get('/json', function routeHandler(req, res) {
 
   res.json({"message": response});
 
-
-
-
-
-
 });
+
+
+
+
 
 console.log("Hello World");
 
