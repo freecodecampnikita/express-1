5ad34f227c84f6c3720990266f250cbb8f153c21
appsaprinsky <app1.n.saprinsky@gmail.com>
2022-03-29 20:33:54 +0600

    Part9

--- myApp.js
+++ myApp.js
@@ -23,8 +23,21 @@ app.get('/now', function(req, res, next) {
   res.send({
     time: req.time
   });
-  
+
 });
+
+
+
+
+app.get('/:word/echo', (req, res) => {
+  const {word} = req.params
+  res.json({"echo": word});
+
+
+});
+
+
+
 
 
 
