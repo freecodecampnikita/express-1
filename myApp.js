8587878d55120889cb204443c197b811bf793b54
appsaprinsky <app1.n.saprinsky@gmail.com>
2022-03-29 20:53:36 +0600

    Part10

--- myApp.js
+++ myApp.js
@@ -15,6 +15,24 @@ app.use( function middleware(req, res, next) {
 
 
 
+app.get("/name", function(req, res) {
+  var firstName = req.query.first;
+  var lastName = req.query.last;
+  // OR you can destructure and rename the keys
+  var { first: firstName, last: lastName } = req.query;
+  // Use template literals to form a formatted string
+  res.json({
+    name: `${firstName} ${lastName}`
+  });
+});
+
+
+
+
+
+
+
+
 app.get('/now', function(req, res, next) {
   req.time = new Date().toString();  // Hypothetical synchronous operation
   next();
